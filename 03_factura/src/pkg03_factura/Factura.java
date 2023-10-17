ffeb451636001dfcf98802e46960d6bcb488ba86
Gerardo Portillo <gerardo.portillo@unah.edu.hn>
2023-10-17 09:14:58 -0600

    commit 171020239021

--- 03_factura/src/pkg03_factura/Factura.java
+++ 03_factura/src/pkg03_factura/Factura.java
@@ -4,6 +4,7 @@ package pkg03_factura;
  * Representa una Factura
  * @author Gerardo Portillo
  */
+import java.util.Calendar;  //clase para el manejo de fechas
 public class Factura {
     //atributos
     public int numero;
@@ -44,11 +45,55 @@ public class Factura {
     }
     
     public void imprimir(){
+        float subtotal = 0.00f;
+        
         System.out.println("********** Factura **********");
         System.out.println("Numero: "+this.numero);
         System.out.println("Fecha: "+this.fecha);
         //si cliente es null entonces es consumidor final y no imprimir rtn
         //si cliente no es null imprimir nombre del cliente y su rtn
+        System.out.print("Cliente: ");
+        if( this.cliente == null )
+            System.out.println("Consumidor Final");
+        else{
+            System.out.println(this.cliente.nombre);
+            System.out.println("\tRTN: "+this.cliente.rtn);
+        }
+        //tabla de productos
+        System.out.println("Producto\tPrecio");
+        //el producto1 al ser obligatorio no viene null
+        System.out.print(this.producto1.nombre);
+        System.out.println("\t"+this.producto1.precioVenta);
+        subtotal += this.producto1.precioVenta;
         
+        //producto2
+        if( this.producto2 == null )
+            System.out.println("-\t0.00");
+        else{
+            System.out.print(this.producto2.nombre);
+            System.out.println("\t"+this.producto2.precioVenta);
+            subtotal += this.producto2.precioVenta;
+        }
+        //producto3
+        if( this.producto3 == null )
+            System.out.println("-\t0.00");
+        else{
+            System.out.print(this.producto3.nombre);
+            System.out.println("\t"+this.producto3.precioVenta);
+            subtotal += this.producto3.precioVenta;
+        }
+        //producto4
+        if( this.producto4 == null )
+            System.out.print("-\t0.00");
+        else{
+            System.out.print(this.producto4.nombre);
+            System.out.println("\t"+this.producto4.precioVenta);
+            subtotal += this.producto4.precioVenta;
+        }
+        
+        //sumario
+        System.out.println("Sub-total: "+subtotal);
+        System.out.println("ISV 15%: "+subtotal*0.15);
+        System.out.println("Total: "+subtotal*1.15);
     }
 }
--- 03_factura/src/pkg03_factura/Main.java
+++ 03_factura/src/pkg03_factura/Main.java
@@ -1,8 +1,28 @@
 package pkg03_factura;
+
 public class Main {
     public static void main(String[] args) {
-        Producto p1 = new Producto("Coca Cola 3L", null, 45);
+        CategoriaProducto cp1 = new CategoriaProducto("Bebidas", "1A");
+        CategoriaProducto cp2 = new CategoriaProducto("Vegetales", "6B");
         
+        Producto p1 = new Producto("Refresco 3L", cp1, 45);
+        Producto p2 = new Producto("Agua Litro", cp1, 20);
+        Producto p3 = new Producto("Tomate Lb", cp2, 21);
+        Producto p4 = new Producto("Lechuga Unidad", cp2, 14);
+        
+        Cliente c1 = new Cliente("Filomeno Colinas", 123, 77777, "no tiene");
+        
+        Factura f1 = new Factura(1, "2023-13-10", null, p1);
+        f1.producto2 = p2;
+        f1.producto3 = null;
+        f1.producto4 = p2;
+        f1.imprimir();
+        
+        Factura f2 = new Factura(2, "2023-13-10", c1, p2);
+        f2.producto2 = p4;
+        f2.producto3 = p3;
+        f2.producto4 = p1;
+        f2.imprimir();
     }
     
 }
